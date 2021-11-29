d810fc63f0ab668284c70ff046af018fa2e5f99a
Zander <alexander.vonneudegg@ucalgary.ca>
2021-11-29 21:30:31 +1300

    simplified

--- scanArr.py
+++ scanArr.py
@@ -5,82 +5,7 @@ import matplotlib.pyplot as plt
 
 angles = []
 times = []
-
-
-def getChange(arr):
-	out = []
-	i = 1
-	while i < len(ball_arr):
-		dif = ball_arr[i] - ball_arr[i - 1]
-		if dif > 200:
-			dif = 360 - dif
-		if dif < - 200:
-			dif = 360 + dif
-		out.append(dif)
-		i += 1
-
-	return out
-
-
-def getDirection(arr):
-	pos = 0
-	neg = 0
-	for i in arr:
-		if i > 0:
-			pos += 1
-		else:
-			neg += 1
-
-	out = arr
-	if pos < neg:
-		out = [-x for x in arr]  # invert entire array if neg
-
-	print("pos", pos, " neg", neg)
-
-	return out
-
-
-def clearNeg(arr):
-	out = []
-	for i in arr:
-		if i < 0:
-			i = -1
-		out.append(i)
-
-	return out
-
-
-def clearHighTEMP(arr):
-	out = []
-	for i in arr:
-		if i > 100:
-			i = -1
-		out.append(i)
-
-	return out
-
-
-def trimSD(arr):
-	out = []
-
-	SDlim = 1
-
-	i = 0
-	while i < len(arr) - 10:
-
-		std = np.std(arr[i:i + 10])
-		mean = np.mean(arr[i:i + 10])  # mean throws error
-
-		for y in arr[i:i + 10]:
-			if mean - (std * SDlim) < y < mean + (std * SDlim):
-				out.append(y)
-			else:
-				out.append(-1)
-
-		i += 10
-
-	return out
-
+timesSmooth = []
 
 def timeForRev(arr, angin):
 	frames = -1
@@ -106,14 +31,17 @@ def timeForRev(arr, angin):
 			frames = len(arr) - j
 			break
 
-	print(frames, " arr ", [int(x) for x in arr[-140:]], " ang ", int(angin))
+	# print(frames, " arr ", [int(x) for x in arr[-140:]], " ang ", int(angin))
 
 	return frames
 
 
 def scan(ang):
 	angles.append(ang)
-	times.append(timeForRev(angles, ang))
+	frames = timeForRev(angles, ang)
+
+	times.append(frames)
+	# timesSmooth.append(avgSlope(times))
 
 
 with open(path + fileOut, 'r') as fd:
@@ -123,31 +51,21 @@ with open(path + fileOut, 'r') as fd:
 		lines = [float(x[1:-1]) for x in lines]
 		ball_arr = ball_arr + lines
 
-# a = getChange(ball_arr)
-# b = getDirection(a)
-# c = clearNeg(b)
-# d = clearHighTEMP(c)
-# e = timePreRev(d)
-
-
-# print(display)
-
-
 i = 1
 while i < len(ball_arr):
 	scan(ball_arr[i])
 
 	i += 1
 
-displayBefore = [x / 5 for x in angles]
-displayAfter = times # trimSD(times)
+displayBefore = times
+displayAfter = timesSmooth
+
 plt.plot([x for x in range(len(displayBefore))], displayBefore, '.')
-plt.plot([x for x in range(len(displayAfter))], displayAfter, '.')  # , plt.ylim([0, 50])
-# plt.plot(times, [x/10 for x in angles[0:-1]], '-')
+plt.plot([x for x in range(len(displayAfter))], displayAfter, '.')
 
 plt.show()
 #
 
-# look at kalman filter
-# shutter effect makes a non-sin wave, really hard to avoid
-# probably use time since last in that position to negate shutter effect
+
+# find drop point, and all the velocities that it crosses this point before
+# then you know key velocities to look out for
